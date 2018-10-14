21284ff381d35ae95c89d507e808c6fc83eae2bf
Shota Nagayama <shota.nagayama@mercari.com>
2018-10-15 00:25:18 +0900

    arbitrary length and repeatable version

--- gottesman_chuang/bob.py
+++ gottesman_chuang/bob.py
@@ -1,5 +1,6 @@
 from SimulaQron.cqc.pythonLib.cqc import *
 from SimulaQron.quantum_digital_signature.gottesman_chuang.quantum_one_way_function import *
+import itertools as it
 
 def fredkin(a, b, c):
     # T* = rot_Z(224)
@@ -38,34 +39,48 @@ def swap_test(fket, fket_, node):
 def main():
     threshold = 0.01
     with CQCConnection("Bob") as Bob:
-        data = list(Bob.recvClassical())
-        print("data Bob received:" + repr(data))
-        keylen_per_mbit = data[0]
-        msg_len = data[1]
+        while True:
+            data = list(Bob.recvClassical())
+            print("data Bob received:" + repr(data))
+            keylen_per_mbit = data[0]
+            msg_len = data[1]
+            msg_ascii_binary = data[2:msg_len+2]
+            """
+            print(msg_ascii_binary)
+            msg_ascii_binary[-1] ^= 1
+            print(msg_ascii_binary)
+            """
+            msg = [chr(int(
+                "".join([str(m) for m in msg_ascii_binary[i:i+8]]),
+                2))
+                       for i in range(0, len(msg_ascii_binary), 8)]
+            k = data[msg_len+2:]
+    
+            fket = []
+            for _ in range(msg_len):
+                fket_ = [[], []]
+                for _ in range(keylen_per_mbit):
+                    fket_[0].append(Bob.recvQubit())
+                for _ in range(keylen_per_mbit):
+                    fket_[1].append(Bob.recvQubit())
+                fket.append(fket_)
+    
+            print("Bob retrived the message msg={} from Alice.".format(msg))
 
-        fket = [[], []]
-        for _ in range(msg_len * keylen_per_mbit):
-            fket[0].append(Bob.recvQubit())
-        for _ in range(msg_len * keylen_per_mbit):
-            fket[1].append(Bob.recvQubit())
+            fdashket = []
+            fdashket = [qubit(Bob) for _ in range(keylen_per_mbit*msg_len)]
+            assert(len(k) == len(fdashket),
+                   "%d, %d" % (len(k), len(fdashket)))
+            stabilizer_states(k, fdashket)
 
-        data = list(Bob.recvClassical())
-        print("data Bob received:" + repr(data))
-        msg = data[:msg_len]
-        k = data[msg_len:]
-
-        fdashket = [qubit(Bob) for _ in range(keylen_per_mbit)]
-        stabilizer_states(k, fdashket)
-
-        retval = swap_test(fket[msg[0]], fdashket, Bob)
-        #retval = swap_test(fket[0], fdashket, Bob)
-
-        if retval < threshold:
-            print("valid")
-        else:
-            print("invalid")
-
-        print("Bob retrived the message msg={} from Alice.".format(msg))
-
+            fket_to_verify = list(it.chain.from_iterable([fket_[m] for fket_, m in zip(fket, msg_ascii_binary)]))
+            retval = swap_test(fket_to_verify, fdashket, Bob)
+            #retval = swap_test(fket[0], fdashket, Bob)
+    
+            if retval < threshold:
+                print("valid")
+            else:
+                print("invalid")
+    
 main()
 
